f6ecb90b126835fb85088246a3defd629da1647f
iTeffa <97382411+lifesheets@users.noreply.github.com>
2025-04-21 15:38:49 +0300

    Retrieves the value of a cookie by its key.

--- src/Cookie.php
+++ src/Cookie.php
@@ -1,8 +1,29 @@
 <?php
 
-namespace Flowaxy\Cookie;
+namespace Flowaxy;
+
+/**
+ * Class Cookie
+ *
+ * A static utility class for managing HTTP cookies.
+ * 
+ * Provides methods to get, set, remove, and check the existence of cookies.
+ *
+ * @package Flowaxy
+ */
 
 class Cookie
 {
+    /**
+     * Retrieves the value of a cookie by its key.
+     *
+     * @param string $key The name of the cookie to retrieve.
+     * @return string|false Returns the cookie value if it exists, or false otherwise.
+     */
+
+    public static function get(string $key): string|false
+    {
+        return $_COOKIE[$key] ?? false;
+    }
 
 }
